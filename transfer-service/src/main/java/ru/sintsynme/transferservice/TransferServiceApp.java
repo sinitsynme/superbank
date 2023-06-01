c7b89f2840d3ddc8f13bd96b7af82f9ba98b3292
sinitsynme <sinitsynme@outlook.com>
2023-06-01 14:34:46 +0400

    Initial commit

---
+++ transfer-service/src/main/java/ru/sintsynme/transferservice/TransferServiceApp.java
@@ -0,0 +1,12 @@
+package ru.sintsynme.transferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferServiceApp.class, args);
+    }
+}
